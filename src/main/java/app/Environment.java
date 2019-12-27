7038a7290968a23f1fd9e7ae0007bbc03a57756b
Evan Troop <evantroop@gmail.com>
2019-12-27 17:04:37 -0500

    Unirest Functionality Tests
    I got the unirest functionality working and ran a test pull from the API I am planning on using.

--- src/main/java/app/Environment.java
+++ src/main/java/app/Environment.java
@@ -1,21 +1,44 @@
 package app;
 
+//Javafx imports
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+//Unirest Imports
+import kong.unirest.Unirest;
+import kong.unirest.HttpResponse;
+import kong.unirest.json.JSONObject;
 
 public class Environment extends Application{
+	public static String BASE = "USD";
 
 	public void start(Stage mainStage){
-		Scene s = new Scene(new BorderPane());
+		BorderPane mainPane = new BorderPane();
 
+		try{
+			HttpResponse<String> response = Unirest.get("https://api.exchangeratesapi.io/latest")
+													.queryString("base", Environment.BASE)
+													.asString();
+			String text = response.getBody();
+			System.out.println(text);
+			JSONObject js =  new JSONObject(text);
+
+			System.out.println(js.getJSONObject("rates").get("CAD"));
+
+		} catch (Exception e){
+			System.out.println(e.getMessage());
+		}
+
+		Scene s = new Scene(mainPane);
 		mainStage.setScene(s);
 		mainStage.show();
 	}
 
 	public static void main(String[] args) {
-		System.out.println("HELLO");
 		launch(args);
 	}
 
