ba67d3dd39f8826d718c80ad185607a8d6ad5adb
Evan Troop <evantroop@gmail.com>
2019-12-27 10:27:52 -0500

    Setup
    I set up a Maven architechture, and added the proper dependancies.
    
    RIght now the default JavaFX Application runs but I cannot package the Project into an executable JAR. I also havent messed with any of the API interaction, I am trying to just get the front end ready for development before I write any of the actual code.

---
+++ src/main/java/app/Environment.java
@@ -0,0 +1,19 @@
+package app;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+public class Environment extends Application{
+
+	public void start(Stage mainStage){
+		Scene s = new Scene(new BorderPane());
+
+		mainStage.setScene(s);
+		mainStage.show();
+	}
+
+
+
+}
