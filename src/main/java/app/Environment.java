77ddfffc1f49f5ee83e5fd642254f33f1010c31e
Evan Troop <evantroop@gmail.com>
2019-12-27 11:22:06 -0500

    Maven Setup Complete
    I have the POM correctly created. Packaging 'app' creates a project that packages everything together and rund the javafx application starting in Environment.
    
    I had to create a helper main because when I used Envoronment.java as the main class, the jar wouldnt run because there were 'missing javafx runtime components.'
    I did some digging and found that this was a problem only if the mainClass was the javaFX application 'main,' and not if it was a class that didn't require any javafx. The helper class allowed essentially a workaround.

--- src/main/java/app/Environment.java
+++ src/main/java/app/Environment.java
@@ -14,6 +14,10 @@ public class Environment extends Application{
 		mainStage.show();
 	}
 
+	public static void main(String[] args) {
+		System.out.println("HELLO");
+		launch(args);
+	}
 
 
 }
---
+++ src/main/java/app/Helper.java
@@ -0,0 +1,7 @@
+package app;
+
+public class Helper{
+	public static void main(String[] args){
+		Environment.main(args);
+	}
+}
